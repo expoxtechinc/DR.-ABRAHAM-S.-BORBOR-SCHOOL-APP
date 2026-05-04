c5a7ad74819e7c5086f8b5ab24605e40f3d03318
expoxtechinc <aki.sokpah.link@gmail.com>
2026-05-04 13:38:23 +0000

    Implement WebView in MainActivity for website loading

---
+++ MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dasbmse.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // Your school website (replace if needed)
+        webView.loadUrl("https://dasbmsoe-official.vercel.app")
+
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+}
